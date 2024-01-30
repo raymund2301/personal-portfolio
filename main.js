905c813fcba586660041c1c4fcaf865201ee8b80
Dnumyar <raymund.dimaandal@gmail.com>
2024-01-30 15:47:58 +0800

    no message

---
+++ main.js
@@ -0,0 +1,7 @@
+    var typed = new Typed(".text",{
+        strings: ["Frontend Developer","Web Designer","Web Developer"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 1000,
+        loop: true
+    });
